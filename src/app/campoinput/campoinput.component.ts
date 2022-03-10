52b6e99e97f2fdf3023abc4748f692fc35a999dd
pberlanda <paolo.berlanda@marconirovereto.it>
2022-03-10 23:01:39 +0100

    2 files modified

--- src/app/campoinput/campoinput.component.ts
+++ src/app/campoinput/campoinput.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+// @Component è un decorator, serve a specificare a angular che questa
+// è una classe di tipo componente e indica il selector, e file html e css correlati
+// sono dei metadata necessari ad angular per definire il componente
 @Component({
   selector: 'app-campoinput',
   templateUrl: './campoinput.component.html',
