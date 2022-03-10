b79c533df1e71a42039c6b91b985a68a1f997c36
pberlanda <paolo.berlanda@marconirovereto.it>
2022-03-10 22:23:50 +0100

    Initial commit

---
+++ src/app/campoinput/campoinput.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-campoinput',
+  templateUrl: './campoinput.component.html',
+  styleUrls: ['./campoinput.component.css'],
+})
+export class CampoinputComponent implements OnInit {
+  name: string = '_____pb_____';
+  constructor() {}
+
+  ngOnInit() {}
+}
