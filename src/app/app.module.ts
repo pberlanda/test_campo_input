f7bb0c9d2950b39c03f9b15d452fcec3cde3511e
pberlanda <paolo.berlanda@marconirovereto.it>
2022-04-04 20:24:41 +0200

    correzioni dopo tentativo fallito di clone in webstorm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { CampoinputComponent } from "./campoinput/campoinput.component";
-import { ServersComponent } from "./servers/servers.component";
+import { CampoinputComponent } from './campoinput/campoinput.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, CampoinputComponent, ServersComponent ],
-  bootstrap:    [ AppComponent ] // elenca tutti i componenti che Angular deve conoscere all'avvio
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, CampoinputComponent],
+  bootstrap: [AppComponent], // elenca tutti i componenti che Angular deve conoscere all'avvio
 })
-export class AppModule { }
+export class AppModule {}
